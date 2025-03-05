eaae5db99301ffd43cb53615a2c6357c2abd3cbf
Devin Zebertavage <60449275+dzebertavage@users.noreply.github.com>
2025-03-05 18:34:40 -0500

    Create factory function for player creation & test

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Test");
+let openSquares = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const createPlayer = (name, letter) => {
+    return { name, letter };
+};
+
+const jeff = createPlayer('Jeff', 'O');
+const mary = createPlayer('Mary', 'X');
+
